96d8a54ad12f2575abec38fa124f3694c9327b41
zakottewell <zakottewell@virginmedia.com>
2024-04-29 22:50:42 +0100

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,19 +1,7 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-            // DISCORD
-
-			DISCORD_TOKEN: string;
-            DISCORD_CLIENT_ID: string;
-
-			// API
-
-            API_KEY: string;
-			API_PORT: number;
-
-			// OTHER
-
-			DATABASE_URL: string;
+			
 		}
 	}
 }
